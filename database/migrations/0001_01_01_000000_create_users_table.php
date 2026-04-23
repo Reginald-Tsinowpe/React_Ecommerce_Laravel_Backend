875127e043bed6b5eb8ce3c2897a1bc5a3209c08
Reginald Tsinowpe <reginaldtsinowpe05@gmail.com>
2026-04-23 12:49:58 +0000

    user model and db change done - ready for auth logic

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('user_name')->unique;
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum("user_type", ["user", "admin"]);
+            $table->boolean("deleted")->default(false);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
