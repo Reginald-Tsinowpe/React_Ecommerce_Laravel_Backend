88cb855aed9f736e36beac27fad16c3697b90a4e
Reginald Tsinowpe <reginaldtsinowpe05@gmail.com>
2026-04-23 13:18:16 +0000

    published cors, added api.php and sanctum for api authentication, modified migrations and added new migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
+use Laravel\Sanctum\HasApiTokens;
+
 return new class extends Migration
 {
+    use HasApiTokens; // , HasFactory, Notifiable;
     /**
      * Run the migrations.
      */
